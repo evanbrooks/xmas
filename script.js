e64f11544b758078b80e6a76e8542248804c140e
Evan Brooks <brooks.ev@gmail.com>
2013-12-25 01:34:35 -0800

    whoops

--- script.js
+++ script.js
@@ -30,10 +30,10 @@ function tilt_detect(event) {
 
 function move_message() {
   if (isNaN(compass)) compass = 300;
-  // $msg.css({
-  //   "-webkit-transform":
-  //     /*"rotate(" + -roll + "deg)*/ "translate3d(" + msg_progress(compass) +  "px, " + (0.01 * (tilt - 90)) + "%, 0)"
-  // });
+  $msg.css({
+    "-webkit-transform":
+      /*"rotate(" + -roll + "deg)*/ "translate3d(" + msg_progress(compass) +  "px, " + (0.01 * (tilt - 90)) + "%, 0)"
+  });
 }
 
 function normalize_degrees(degrees) {
