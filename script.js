39891ab5d20fdc71f8bdf65c42ffb2700c0c4a78
Evan Brooks <brooks.ev@gmail.com>
2013-12-24 23:18:58 -0800

    move message in sync with compass

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ var tilt = -3,
     compass = 0;
 
 var $msg;
+var msg_width = 0;
 
 function tilt_detect(event) {
-  compass = parseInt( event.alpha * 1 ) / 1;
+  compass = event.alpha;
   tilt    = parseInt( event.beta  * 1 ) / 1;
   roll    = parseInt( event.gamma * 1 ) / 1;
 }
@@ -14,13 +15,26 @@ function move_message() {
   if (isNaN(compass)) compass = 0;
   $msg.css({
     "-webkit-transform":
-      "rotate(" + -roll + "deg) translate3d(" + compass +  "%, " + (tilt - 80) + "%, 0)"
+      "rotate(" + -roll + "deg) translate3d(" + msg_progress(compass) +  "px, " + (tilt - 80) + "%, 0)"
   });
+}
+
+function msg_progress(degrees) {
+  if (degrees < 0) {
+    return msg_progress(degrees + 360)
+  }
+  else if (degrees > 360) {
+    return msg_progress(degrees - 360)
+  }
+  else {
+    return -1 * (degrees / 360) * msg_width;
+  }
 }
 
 
 $(function(){
   $msg = $("#message");
+  msg_width = $msg.width();
   window.addEventListener("deviceorientation", tilt_detect, true);
 
   (function step(){
